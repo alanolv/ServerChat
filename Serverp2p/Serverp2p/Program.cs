c90e0522b765bb02d6da16afe0b087988c501dcb
alanolv <olvera.cristino.alan@gmail.com>
2023-10-25 01:17:35 -0600

    Feat:Se crea el servidor

--- Serverp2p/Serverp2p/Program.cs
+++ Serverp2p/Serverp2p/Program.cs
@@ -1,1 +1,71 @@
-﻿
+﻿using System; 
+using LibreriaNodos;
+using System.Threading;
+
+
+namespace Serverp2p {
+
+    public class Program {
+
+        
+        //atributos
+        static int serverPuerto = 4405;
+        static string serverIpAdress = "localHost";
+        static string userServerName = "Server";
+        static Thread Tlisten, Tbroadcast;
+
+        static User serverUser = new User(userServerName, serverIpAdress, serverPuerto);
+
+        static Server server = new Server(serverUser);
+
+        
+
+        public static void Main(string[] args)
+        {
+
+            Program program = new Program();
+            server.start();
+            Tlisten = new Thread(server.Listen);
+            Tlisten.Start();
+            
+            Tbroadcast = new Thread(serverBroadcast);
+            Tbroadcast.Start();
+            
+
+
+
+    
+
+            
+
+
+
+            
+        }
+
+        public static void serverBroadcast()
+        {
+            
+            while (true)
+            {
+                try
+                {
+                    if (server.GetUsers() != null)
+                    {
+
+                        server.ServerBroadcastUserOnlineList();
+                        Thread.Sleep(5000);
+
+                    }
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+                
+            }
+
+        }
+
+    }
+}
